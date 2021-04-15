f311962059e51b4bc34007116bf5a858d536f02c
unknown <alexey.moroz87@gmail.com>
2021-04-15 09:17:42 +0300

    внес правки

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <? if($show_complete_tasks == 1):?> checked <? endif; ?>>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?= ($show_complete_tasks) ? 'checked' : ''; ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -93,7 +93,7 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <?if ($show_complete_tasks == 1) :?>
+                    <?if ($show_complete_tasks === 1) :?>
                     <tr class="tasks__item task task--completed">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
